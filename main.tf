a1a3715dfa5de4b7b55a24cf3cf31e02b3647f72
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2020-10-01 07:59:44 +0200

    Remove display of key

--- main.tf
+++ main.tf
@@ -8,10 +8,3 @@ provider "null" {
 resource "tls_private_key" "key" {
   algorithm = "ECDSA"
 }
-
-resource "null_resource" "display" {
-  triggers = {
-    pubkey  = tls_private_key.key.public_key_pem
-    privkey = tls_private_key.key.private_key_pem
-  }
-}
